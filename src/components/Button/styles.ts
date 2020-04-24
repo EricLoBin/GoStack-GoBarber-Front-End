d25b73d4a5f631cb2f9cf645b7cb679b607333ed
EricLoBin <ekilobin@gmail.com>
2020-04-24 10:56:08 -0300

    [UPDATE] Usabilidade do input

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -15,7 +15,8 @@ export const Container = styled.button`
     margin-top: 16px;
     transition: background-color 0.2s;
 
-    &:hover {
+    &:hover,
+    &:focus {
         background: ${shade(0.2, '#ff9000')};
     }
 
