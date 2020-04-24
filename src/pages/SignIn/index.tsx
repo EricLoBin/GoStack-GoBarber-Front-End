56ac805a43804a10591fbc1bc3b87cd18264a0d7
EricLoBin <ekilobin@gmail.com>
2020-04-24 11:56:36 -0300

    [UPDATE] Validando cadastro

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -16,7 +16,7 @@ const SignIn: React.FC = () => (
             <form>
                 <h1>Faça seu logon</h1>
 
-                <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
+                <Input name="email" icon={FiMail} placeholder="E-mail" />
 
                 <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
 
