4dc0ccfd14d3083d37e0ed892bc968c465cb1ac7
EricLoBin <ekilobin@gmail.com>
2020-04-30 11:52:49 -0300

    [UPDATE] Configurando Rotas

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -4,6 +4,7 @@ import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 import getValidationErrors from '../../utils/getValidationErrors'
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -52,10 +53,10 @@ const SignUp: React.FC = () => {
                 <Button type="submit">Cadastrar</Button>
             </Form>
 
-            <a href="login">
+            <Link to="/">
                 <FiArrowLeft />
                 Voltar para logon
-            </a>
+            </Link>
         </Content>
     </Container>
     );
